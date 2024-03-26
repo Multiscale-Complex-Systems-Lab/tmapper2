fb26f1d9b34e4421adf4befaa36c4b446ce89837
Mengsen Zhang <mengsen@host-259562.dhcp.egr.msu.edu>
2024-03-26 19:18:09 -0400

    add more documentations

--- tmapper_demo.m
+++ tmapper_demo.m
@@ -1,5 +1,14 @@
 % tmapper_demo.m
 % ---------------
+% This script demonstrates how to use Temporal Mapper to construct network
+% representations of time series data.
+% For technical details, see the published paper: https://doi.org/10.1162/netn_a_00301
+% 
+% Citation:
+%   Mengsen Zhang, Samir Chowdhury, Manish Saggar (2023). Temporal Mapper:
+%   Transition networks in simulated and real neural dynamics. Network
+%   Neuroscience, 7(2): 431–460. doi: https://doi.org/10.1162/netn_a_00301  
+% 
 % Sample data: historic East Lansing weather. 
 % Source: https://kilthub.cmu.edu/articles/dataset/Compiled_daily_temperature_and_precipitation_data_for_the_U_S_cities/7890488?file=32874086
 
@@ -15,14 +24,14 @@ close all
 clc
 
 dat = readtable('EL_temp.csv');
-dat = rmmissing(dat);
+dat = rmmissing(dat);% remove missing data
 dat = dat(53884:end,:);% just take some recent data for demo
 %% ===== quick look at data =====
 % --- pick a couple of variable to define the state
 varidx = 3:5;
-X = zscore(dat{:,varidx}); 
+X = zscore(dat{:,varidx}); % z-score to keep different dimensions more comparable, not always necessary
 
-% --- the time
+% --- define time
 t = dat.Date;
 
 % --- names of the variables
@@ -63,26 +72,32 @@ cb.Label.String = "L-" + p + " distance";
 % --- tmapper construction parameters
 k = 3;
 d = 3; % compression rate. loops below this parameter is absorbed into nodes
-texclude = 30; % a temporal neighborhood where you consider things are not recurring. (in unit of time points)
+texclude = 30; % a temporal neighborhood where you consider things are not recurring. usually 1 is good. (in unit of time points)
+
 % --- define a variable for coloring the graph
 colorvarname = 'tmax';
 colorvar = dat{:,colorvarname};
 
-% --- construct knn graph
+% --- step 1: construct knn graph
 disp("computing knn graph")
-tidx = (1:length(t))';
+tidx = (1:length(t))';% these indices will be used to define temporal neighborhoods
 tic
 g = tknndigraph (D,k,tidx,'reciprocal',true,'timeExcludeSpace', true,'timeExcludeRange',texclude);
 toc
 
-% --- construct simplified graph
+% --- step 2: construct simplified graph
+% g_simp is the graph that we want
+% members tell you each node is mapped to which time points
 disp("simplifying knn graph")
 tic
 [g_simp, members, nodesize, D_simp] = filtergraph(g,d,'reciprocal',true);
 toc
 
 
-% --- show transition networks      and recurrence plot
+% --- show transition networks and recurrence plot
 [a1,a2,~,~,hg,D_geo] = plotgraphtcm(g_simp,colorvar,t,members,'nodesizerange',[1,10],...
     'colorlabel',colorvarname,'labelmethod','median','nodesizemode','log');
 title(a1,["sample data","k=" + k + ", d=" + d])
+%% ===== P.S. =====
+% CycleCount2p can be used to calculate the cycles in the graph, which can
+% be quite fun to explore. 
